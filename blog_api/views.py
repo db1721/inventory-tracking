3a4ffca60bdd1830b8182c70a5a5466b524def8a
Dan Beck <63600571+db1721@users.noreply.github.com>
2024-03-11 23:05:03 -0400

    Started project

---
+++ blog_api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.post_objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    pass
